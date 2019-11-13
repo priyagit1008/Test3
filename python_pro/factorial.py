c52f8c36e5e883e28cf9811ae5e83a21586d43a4
priya1008 <priyapatil20191l@gmail.com>
2019-11-13 21:27:43 +0530

    created binary.py file

--- python_pro/factorial.py
+++ python_pro/factorial.py
@@ -1,6 +1,9 @@
-n=input("Enter a num")
-if n==0 or n==1:
-	print("Enter a valid input")
-else:
-	fact=n*(n-1)
-	print("factorial of num is",num ,fact(num))
+# n=input("Enter a number")
+
+# if n==0 or n==1:
+# 	print("Enter a input")
+
+# # else:
+
+# # 	fact=n*(n-1)
+# # 	print("factorial of num is",num ,fact(num))
